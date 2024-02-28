9fb33b9136c28ac2892d4e99f8efcde9c10b9fc3
Jaswantsuthar <95173617+Jaswantsuthar@users.noreply.github.com>
2024-02-28 22:18:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+	let inpts=document.queryselector("input")
+	let name=inpts[0].value
+	let last=inpts[1].value
+	let fullname=name + " "+ last
+	alert(fullname)
+	
 }
